36ec64468979fafe6ffb5bfa7a5f310a7c033488
unknown <henritein@gmail.com>
2020-11-30 22:17:42 +0200

    final ultra max

--- js/gallery.js
+++ js/gallery.js
@@ -1,26 +1,24 @@
-let galleryimages = document.querySelectorAll(".gallery-img");
-let getLatestOpenedImg;
+let galleryimages = document.querySelectorAll(".gallery-img");/*Loeb muutujasse ainult kindlat tüüpi */
 let windowWidth = window.innerWidth;
 
 if (galleryimages) {
     galleryimages.forEach(function(image, index) {
             image.onclick = function(){
                 let getElementCss= window.getComputedStyle(image);
-                let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+                let getFullImgUrl = getElementCss.getPropertyValue("background-image"); /*võtab aadressi, millele on klikitud*/
                 let getImgUrlPos = getFullImgUrl.split("/img/thumbs/");
-                let setNewImgUrl = getImgUrlPos[1].replace('")', '');
+                let setNewImgUrl = getImgUrlPos[1].replace('")', '');/*Eemaldab mitte vajalikud asjad.*/
                 
-                getLatestOpenedImg = index +1;
                 
                 let container = document.body;
-                let newImgWindow= document.createElement("div");
+                let newImgWindow= document.createElement("div"); /*Teeb uue muutuja*/
                 container.appendChild(newImgWindow );
-                newImgWindow.setAttribute("class","img-window")
-                newImgWindow.setAttribute("onclick","closeImg()")
+                newImgWindow.setAttribute("class","img-window") /*Annab klassi*/
+                newImgWindow.setAttribute("onclick","closeImg()") /*Klikkides kasutab funktsiooni "closeimg"*/
 
                 let newImg = document.createElement("img");
                 newImgWindow.appendChild(newImg);
-                newImg.setAttribute("src", `media/img/${setNewImgUrl}`)
+                newImg.setAttribute("src", "media/img/" +setNewImgUrl)
 
             }
 
@@ -28,7 +26,7 @@ if (galleryimages) {
 }
 
 
-function closeImg() {
+function closeImg() { /*Funktsioon ette toodud pildi sulgemiseks*/
     document.querySelector(".img-window").remove();
 
 }
